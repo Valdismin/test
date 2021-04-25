9ce4c13a9f3ef6596ff6047e5ae29807a38e6b86
Valdismin <Valdismin@gmail.com>
2021-04-25 19:12:00 +0300

    Refactor some code.

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -3,12 +3,11 @@ import {Redirect, Route, Switch } from "react-router-dom";
 import { Search } from "../Search/Search";
 import {Bookmark} from "../Bookmark/Bookmark";
 import {Error404} from "../Error404";
-import {Auth} from "../AuthPage/Auth_Page";
+
 
 export const PATH = {
     SEARCH: "/search",
-    BOOKMARK: "/bookmarks",
-    AUTH:"/auth"
+    BOOKMARK: "/bookmarks"
 }
 
 export const Routes = () => {
@@ -19,7 +18,6 @@ export const Routes = () => {
                 <Route path={"/"} exact render={() => <Redirect to={PATH.SEARCH}/>}/>
                 <Route path={PATH.SEARCH} render={() => <Search/>}/>
                 <Route path={PATH.BOOKMARK} render={() => <Bookmark/>}/>
-                <Route path={PATH.AUTH} render={() => <Auth/>}/>
                 у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу
                 <Route render={() => <Error404/>}/>
 
